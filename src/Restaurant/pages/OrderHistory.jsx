a66d6c9b9f1d7c8198eae3d776c681021d88868f
dev05mavyax <dev05@mavyax.com>
2024-08-24 11:32:38 +0530

    updated changes

--- src/Restaurant/pages/OrderHistory.jsx
+++ src/Restaurant/pages/OrderHistory.jsx
@@ -7,7 +7,7 @@ import { IoSearchOutline } from "react-icons/io5";
 const OrderHistory = () => {
   return (
     <RestaurantWrapper>
-      <section className='w-full h-full bg-[#E7EBEF] '>
+      <section className='w-full h-full'>
         <div>
           <div>
             <div className='flex items-center'>
