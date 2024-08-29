ae65401cc2d3eebf011a02ec851ea16cee2cf341
dev05mavyax <dev05@mavyax.com>
2024-08-29 10:24:38 +0530

    some changes sidebar

--- src/Restaurant/components/Sidebar.jsx
+++ src/Restaurant/components/Sidebar.jsx
@@ -6,6 +6,7 @@ import OrderHistory2Img from '../../assets/restaurant-03.png'
 import OffersImg from '../../assets/discount.png'
 import PayoutImg from '../../assets/payment-02.png'
 import ReportingImg from '../../assets/scooter-01.png'
+import Reporting2Img from '../../assets/scooter-02.png'
 import HelpCenterImg from '../../assets/help-circle.png'
 import OutletInfoImg from '../../assets/115799_shop_icon 1.png'
 import ComplaintsImg from '../../assets/profile.png'
@@ -25,33 +26,33 @@ const Sidebar = () => {
                     <img src={OrderImg} alt="" className='w-[24px] h-[24px]' />
                     <span className='text-[#637D92] text-[16.8px] font-normal font-inter group-hover:text-[#1AA6F1]'>Orders</span>
                 </Link>
-                <Link to="/restaurant/menu" className={`flex justify-start items-center gap-[14px] h-[52px] p-[14px] cursor-pointer group hover:bg-[#F3F3FC] ${pathname.includes("/restaurant/menu") ? 'text-[#1AA6F1] bg-[#F3F3FC]' : 'text-[red]'}`}>
+                <Link to="/restaurant/order-menu" className={`flex justify-start items-center gap-[14px] h-[52px] p-[14px] cursor-pointer group hover:bg-[#F3F3FC] ${pathname.includes("/restaurant/order-menu") ? 'text-[#1AA6F1] bg-[#F3F3FC]' : 'text-[red]'}`}>
                     <img src={MenuImg} alt="" className='w-[24px] h-[24px]' />
-                    <span className='text-[#637D92] text-[16.8px] font-normal font-inter group-hover:text-[#1AA6F1]'>Menu</span>
+                    <span className={`${pathname.includes("/restaurant/order-menu") ? 'text-[#637D92] bg-[#F3F3FC]' : 'text-[#637D92] group-hover:text-[#1AA6F1]'} text-[16.8px] font-normal font-inter `}>Menu</span>
                 </Link>
                 <Link to="/restaurant/order-history" className={`flex justify-start items-center gap-[14px] h-[52px] p-[14px] cursor-pointer group hover:bg-[#F3F3FC] ${pathname.includes("/restaurant/order-history") ? 'text-[#1AA6F1] bg-[#F3F3FC]' : 'text-[red]'}`}>
                     <img src={pathname.includes("/restaurant/order-history") ? OrderHistory2Img : OrderHistoryImg} alt="" className='w-[24px] h-[24px]' />
                     <span className={`${pathname.includes("/restaurant/order-history") ? 'text-[#1AA6F1]' : 'text-[#637D92]'} text-[16.8px] font-normal font-inter group-hover:text-[#1AA6F1]`}>Order history</span>
                 </Link>
-                <Link to="/restaurant/offer" className={`flex justify-start items-center gap-[14px] h-[52px] p-[14px] cursor-pointer group hover:bg-[#F3F3FC] ${pathname.includes("/restaurant/offer") ? 'text-[#1AA6F1] bg-[#F3F3FC]' : 'text-[red]'}`}>
+                <Link to="/restaurant/offers" className={`flex justify-start items-center gap-[14px] h-[52px] p-[14px] cursor-pointer group hover:bg-[#F3F3FC] ${pathname.includes("/restaurant/offers") ? 'text-[#1AA6F1] bg-[#F3F3FC]' : 'text-[red]'}`}>
                     <img src={OffersImg} alt="" className='w-[24px] h-[24px]' />
-                    <span className='text-[#637D92] text-[16.8px] font-normal font-inter group-hover:text-[#1AA6F1]'>Offers</span>
+                    <span className={`text-[#637D92] text-[16.8px] font-normal font-inter ${pathname.includes("/restaurant/offers") ? 'text-[#637D92] bg-[#F3F3FC]' : 'text-[#637D92] group-hover:text-[#1AA6F1]'}`}>Offers</span>
                 </Link>
-                <Link to="/restaurant/payout" className={`flex justify-start items-center gap-[14px] h-[52px] p-[14px] cursor-pointer group hover:bg-[#F3F3FC] ${pathname.includes("/restaurant/payout") ? 'text-[#1AA6F1] bg-[#F3F3FC]' : 'text-[red]'}`}>
+                <Link to="/restaurant/payout" className={`flex justify-start items-center gap-[14px] h-[52px] p-[14px] cursor-pointer group hover:bg-[#F3F3FC] ${pathname.includes("/restaurant/payout") ? 'text-[#637D92] bg-[#F3F3FC]' : 'text-[#637D92]'}`}>
                     <img src={PayoutImg} alt="" className='w-[24px] h-[24px]' />
-                    <span className={` text-[16.8px] font-normal font-inter ${pathname.includes("/restaurant/payout") ? 'text-[#1AA6F1]' : 'text-[#637D92]'} group-hover:text-[#1AA6F1]`}>Payout</span>
+                    <span className={`text-[#637D92] text-[16.8px] font-normal font-inter ${pathname.includes("/restaurant/payout") ? 'text-[#637D92] bg-[#F3F3FC]' : 'text-[#637D92] group-hover:text-[#1AA6F1]'}  `}>Payout</span>
                 </Link>
                 <Link to="/restaurant/reporting" className={`flex justify-start items-center gap-[14px] h-[52px] p-[14px] cursor-pointer group hover:bg-[#F3F3FC] ${pathname.includes("/restaurant/reporting") ? 'text-[#1AA6F1] bg-[#F3F3FC]' : 'text-[red]'}`}>
-                    <img src={ReportingImg} alt="" className='w-[24px] h-[24px]' />
-                    <span className='text-[#637D92] text-[16.8px] font-normal font-inter group-hover:text-[#1AA6F1]'>Reporting</span>
+                    <img src={pathname.includes("/restaurant/reporting")?Reporting2Img:ReportingImg}alt="" className='w-[24px] h-[24px]' />
+                    <span className={`${pathname.includes("/restaurant/reporting") ? 'text-[#1AA6F1]' : 'text-[#637D92]'} text-[16.8px] font-normal font-inter group-hover:text-[#1AA6F1]`}>Reporting</span>
                 </Link>
                 <Link to="/restaurant/help-center" className={`flex justify-start items-center gap-[14px] h-[52px] p-[14px] cursor-pointer group hover:bg-[#F3F3FC] ${pathname.includes("/restaurant/help-center") ? 'text-[#1AA6F1] bg-[#F3F3FC]' : 'text-[red]'}`}>
                     <img src={HelpCenterImg} alt="" className='w-[24px] h-[24px]' />
                     <span className='text-[#637D92] text-[16.8px] font-normal font-inter group-hover:text-[#1AA6F1]'>Help Center</span>
                 </Link>
-                <Link to="/restaurant/outlet" className={`flex justify-start items-center gap-[14px] h-[52px] p-[14px] cursor-pointer group hover:bg-[#F3F3FC] ${pathname.includes("/restaurant/outlet") ? 'text-[#1AA6F1] bg-[#F3F3FC]' : 'text-[red]'}`}>
+                <Link to="/restaurant/outlet-info" className={`flex justify-start items-center gap-[14px] h-[52px] p-[14px] cursor-pointer group hover:bg-[#F3F3FC] ${pathname.includes("/restaurant/outlet-info") ? 'text-[#1AA6F1] bg-[#F3F3FC]' : 'text-[red]'}`}>
                     <img src={OutletInfoImg} alt="" className='w-[24px] h-[24px]' />
-                    <span className='text-[#637D92] text-[16.8px] font-normal font-inter group-hover:text-[#1AA6F1]'>Outlet Info</span>
+                    <span className={`text-[#637D92] text-[16.8px] font-normal font-inter ${pathname.includes("/restaurant/outlet-info") ? 'text-[#637D92] bg-[#F3F3FC]' : 'text-[#637D92] group-hover:text-[#1AA6F1]'}`}>Outlet Info</span>
                 </Link>
                 <Link to="/restaurant/complaint" className={`flex justify-start items-center gap-[14px] h-[52px] p-[14px] cursor-pointer group hover:bg-[#F3F3FC] ${pathname.includes("/restaurant/complaint") ? 'text-[#1AA6F1] bg-[#F3F3FC]' : 'text-[red]'}`}>
                     <img src={ComplaintsImg} alt="" className='w-[24px] h-[24px]' />
