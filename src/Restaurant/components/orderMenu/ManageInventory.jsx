f242838ca861bbf70dd6e75ad3ecb171b04d9b24
dev05mavyax <dev05@mavyax.com>
2024-12-05 13:00:58 +0530

    code updated

--- src/Restaurant/components/orderMenu/ManageInventory.jsx
+++ src/Restaurant/components/orderMenu/ManageInventory.jsx
@@ -2,8 +2,11 @@ import { Input } from '@/components/ui/input'
 import React from 'react'
 import { IoSearchOutline } from 'react-icons/io5'
 import ProductInventory from './ProductInventory'
+import ItemComp from './ItemComp'
+import Spinner from '../Spinner'
+import DataNotFound from '../DataNotFound'
 
-const ManageInventory = () => {
+const ManageInventory = ({allCategories}) => {
     return (
         <div>
             <div className='w-[500px] relative mt-4'>
@@ -13,9 +16,9 @@ const ManageInventory = () => {
             <div className="w-full rounded-lg overflow-hidden h-[500px] flex items-start border border-[#CED7DE] my-5">
                 <div className="left-section relative w-1/3 h-full rounded-tl-lg bg-white border-r border-r-[#CED7DE]">
                     <h3 className=" class-base5 p-5 bg-[#F2F4F7] border-b border-b-[#CED7DE]">Categories</h3>
-                    {/* <div className="overflow-y-auto h-full pb-[180px]">
+                    <div className="overflow-y-auto h-full pb-[180px]">
                         {allCategories?.map((category) => (
-                            <ItemComp key={category?._id} />
+                            <ItemComp key={category?._id} category={category} />
                         ))}
 
                         {allCategories.length === 0 && isLoading &&
@@ -25,7 +28,7 @@ const ManageInventory = () => {
                         {allCategories.length === 0 && !isLoading &&
                             <DataNotFound name="Categories" />
                         }
-                    </div> */}
+                    </div>
                 </div>
                 <div className="right-section w-2/3 bg-white h-full">
                     <h3 className=" class-base5 p-5 bg-[#F2F4F7] border-b border-b-[#CED7DE]">Combos (3)</h3>
