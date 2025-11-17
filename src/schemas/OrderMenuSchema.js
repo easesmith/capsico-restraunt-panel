d28e81c592efcaeef8d03e3e31a1d0d2a4d7f34c
dev03lkomavyax <dev03lko@mavyax.com>
2025-11-17 10:41:30 +0530

    ♻️ refactor(schema): simplify category schema
    - make description and isActive optional in category schema

--- src/schemas/OrderMenuSchema.js
+++ src/schemas/OrderMenuSchema.js
@@ -1,14 +1,11 @@
-import { z } from 'zod'
+import { z } from "zod";
 
 export const categorySchema = z.object({
   category: z
     .string()
     .min(3, "Mininum 3 char is required")
     .max(50, "Category should be less than 50 characters"),
-  description: z
-    .string()
-    .min(3, "Mininum 3 char is required")
-    .max(50, "Description should be less than 50 characters"),
+  description: z.string().optional(),
   isActive: z.coerce.boolean().optional(),
 });
 
